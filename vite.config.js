f15a66c1a4c0efc96f73ca049ce31c3f807fe655
fuselierr <mikegao000@gmail.com>
2026-09-09 15:58:04 -0400

    added vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   server: {
     proxy: {
       // Forwards /api/* from the Vite dev server to the Express
